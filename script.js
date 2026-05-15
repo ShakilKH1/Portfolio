03af1aa02c22cbae61b2d3a5462707230a1265bf
ShakilKH1 <mshasim@our.ecu.edu.au>
2026-05-15 23:28:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Portfolio Loaded");
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
